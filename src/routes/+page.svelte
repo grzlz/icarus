3066db27e67d55d9bab6fadd9b3cf5ba85272b5c
grzlz <guillermo.rdzlpz@gmail.com>
2025-05-25 18:44:31 -0600

    Feat: hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
-<main>
-    <h1 >Entregamos software a tiempo</h1>
-</main>
+
+<section class="bg-secondary-900 text-white min-h-[80vh] flex items-center font-primary">
+  <div class="max-w-7xl mx-auto px-6 md:px-10 flex flex-col md:flex-row items-center gap-12">
+    
+    <!-- Text Content -->
+    <div class="flex-1 text-center md:text-left">
+      <h1 class="text-4xl md:text-6xl font-bold leading-tight">
+        Entregamos <span class="text-blue-400">software a tiempo</span>
+      </h1>
+      <p class="mt-6 text-lg md:text-xl text-gray-300 max-w-xl">
+        Diseñamos y construimos tecnología para mejorar al mundo.
+      </p>
+
+      <div class="mt-8">
+        <a href="/contacto" class="bg-red-800 hover:bg-primary-500 text-primary-100 font-semibold px-6 py-3 rounded-xl transition">
+          Entra a scythe
+        </a>
+      </div>
+    </div>
+
+
+  </div>
+</section>
