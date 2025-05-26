419f922fb0643826a4e88b51bb08d98a6afbc40a
grzlz <guillermo.rdzlpz@gmail.com>
2025-05-26 03:37:14 -0600

    Feat: make navbar sticky

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -5,7 +5,7 @@
 </script>
 
 
-<nav class="relative flex items-center justify-between bg-secondary-800  md:px-8 min-h-[60px] px-4 py-2">
+<nav class="sticky top-0 flex items-center justify-between bg-secondary-800  md:px-8 min-h-[60px] px-4 py-2">
     <h1 class="hidden md:block text-primary-200 text-lg font-semibold font-primary">ICARUS</h1>
     
     <div class="absolute left-1/2 transform -translate-x-1/2">
