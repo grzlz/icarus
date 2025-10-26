27400ce1a3ad741b8d8ea26e12f5b72b1002a06a
grzlz <guillermo.rdzlpz@gmail.com>
2025-10-26 05:40:00 -0600

    begin hosting ariadna.sh

---
+++ static/ariadna.sh
@@ -0,0 +1,6 @@
+# Description: .sh installer for ariadna: icarus.mx version of lazyvim
+#
+# TODO: install nvim
+# TODO: modify .config/nvim with our settings
+# TODO: change lazyvim logo for our ariadna-ascii.txt file
+#
