566a8f36260fd613360212f5d20439fe7c516afb
grzlz <guillermo.rdzlpz@gmail.com>
2025-10-26 11:14:54 -0600

    change to tokyo day

--- static/ariadna/lua/plugins/colorscheme.lua
+++ static/ariadna/lua/plugins/colorscheme.lua
@@ -1,17 +1,21 @@
 return {
   {
-    "rebelot/kanagawa.nvim",
+    "folke/tokyonight.nvim",
     priority = 1000,
     opts = {
+      style = "day",  -- Light theme with blue accents
       transparent = true,
-      theme = "lotus", -- light theme
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-lotus",
+      colorscheme = "tokyonight-day",
     },
   },
 }
